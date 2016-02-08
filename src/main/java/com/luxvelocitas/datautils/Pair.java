4c9f29a08d2088587485ea9e94f5cce248131d28
Konrad Markus <konker@luxvelocitas.com>
2016-02-08 18:06:49 +0200

    Add general purpose Pair class

---
+++ src/main/java/com/luxvelocitas/datautils/Pair.java
@@ -0,0 +1,28 @@
+package com.luxvelocitas.datautils;
+
+/**
+ * A general-purpose pair
+ */
+public class Pair<A,B> {
+    public final A a;
+    public final B b;
+
+    public Pair(A aa, B bb) {
+        a = aa;
+        b = bb;
+    }
+
+    @Override
+    public int hashCode() {
+        return a.hashCode() ^ b.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Pair)) return false;
+        Pair rhs = (Pair)o;
+        return this.a.equals(rhs.a) &&
+                this.b.equals(rhs.b);
+    }
+
+}
