994e7c82c44a7657f0afb45ffa85f91c2dcaf736
Konrad Markus <konker@luxvelocitas.com>
2016-02-08 18:07:03 +0200

    Add DummyOutputStream

---
+++ src/main/java/com/luxvelocitas/datautils/DummyOutputStream.java
@@ -0,0 +1,10 @@
+package com.luxvelocitas.datautils;
+
+import java.io.OutputStream;
+
+/**
+ */
+public class DummyOutputStream extends OutputStream {
+    @Override
+    public void write(int b) { /*[NOOP] */ }
+}
